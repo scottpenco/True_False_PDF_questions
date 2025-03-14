b147f0f934867ab2cb88b5c13a85de6c2a414b3d
scottpenco <43186751+scottpenco@users.noreply.github.com>
2025-03-14 13:58:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 # Set OpenAI API Key
 load_dotenv()
-apikey = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
 # Function to extract text from PDF
